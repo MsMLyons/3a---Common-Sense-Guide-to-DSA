9c1cae6e6af1d4810a83513faabce8c5cc8e5333
MsMLyons <ms.markilyons@gmail.com>
2024-01-02 21:35:26 -0500

    Update Chapters 3 & 4

--- Chapter 3/ch3_code_examples.py
+++ Chapter 3/ch3_code_examples.py
@@ -1,25 +1,17 @@
-def bubble_sort(list):
-    # keep track of the unsorted rightmost index of the array
-    unsorted_until_index = len(list) - 1
-    # track whether the array is fully sorted
-    sorted = False
+# Big O = O(N)
+# Example 1
 
-    # loop runs as long as the array is not sorted
-    while not sorted:
-        # assume the array is sorted until a swap is made
-        # remains true if a full loop is made without a swap
-        sorted = True
-        # for each value in the unsorted array:
-        for i in range(unsorted_until_index):
-            # compare adjacent values in the array
-            if list[i] > list[i+1]:
-                # swap the order of values
-                list[i], list[i+1] = list[i+1], list[i]
-                # turn sorted back to false when a swap is made
-                sorted = False
-        # decrement the index value by one each passthrough
-        unsorted_until_index -= 1
-    # return the list/array once sorted
-    return list
+things = ["apples", "cheese", "trees", "porcupines"]
 
-print(bubble_sort([65, 55, 45, 35, 25, 15, 10]))
+for thing in things:
+    print("Here's a thing: " + thing)
+
+# Example 2
+def is_prime(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+# Both examples demonstrate O(N) efficiency because as the number or dataset increases,
+# so do the number of steps necessary to complete each iteration
